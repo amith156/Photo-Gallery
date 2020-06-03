b660a8dffb9f49855f717b898254bd4e43781863
Amith's MacBook <amithkumarnarayan1@gmail.com>
2020-06-03 15:16:59 +0100

    Initial Commit

---
+++ Photo Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Gallery
+//
+//  Created by Amith Kumar Narayan on 03/06/2020.
+//  Copyright © 2020 Narayan inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
